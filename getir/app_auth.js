bf77263e663ed45d7bfeb167010cb974bfcad556
Kerem Güneş <k-gun@mail.com>
2016-02-20 17:37:17 +0200

    Bump unneeded stuff.

--- getir/app_auth.js
+++ getir/app_auth.js
@@ -2,7 +2,4 @@ module.exports = {
    generateAccessToken: function(){
       return (1 + Math.random()).toString(15).substring(2, 2 + 32);
    },
-   validateAccessToken: function(accessToken){
-      // @todo
-   }
 };
