a66b29b61a365f4c5588806d3ca8379f673b51a6
Kerem Güneş <k-gun@mail.com>
2016-02-20 16:22:40 +0200

    Bind Redis object to app.

--- getir/app.js
+++ getir/app.js
@@ -5,5 +5,6 @@ app.Route = require("./app_route.js");
 app.Payload = require("./app_payload.js");
 app.Error = require("./app_error.js");
 app.Database = require("./app_database.js");
+app.Redis = require("./app_redis.js");
 
 module.exports = app;
