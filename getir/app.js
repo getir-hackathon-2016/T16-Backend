6e397d0c14add57bd2a546d65584ae871587f9ec
Kerem Güneş <k-gun@mail.com>
2016-02-20 05:52:03 +0200

    Add app.Error module.

--- getir/app.js
+++ getir/app.js
@@ -3,5 +3,6 @@ var app = {};
 app.Auth = require("./app_auth.js");
 app.Route = require("./app_route.js");
 app.Payload = require("./app_payload.js");
+app.Error = require("./app_error.js");
 
 module.exports = app;
