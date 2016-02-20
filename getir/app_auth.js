b1f9fcf93b1664334ab08d2b70378a1ec2253015
Kerem Güneş <k-gun@mail.com>
2016-02-20 03:14:09 +0200

    Add template function.

--- getir/app_auth.js
+++ getir/app_auth.js
@@ -1,5 +1,8 @@
 module.exports = {
    generateAccessToken: function(){
       return (1 + Math.random()).toString(15).substring(2, 2 + 32);
+   },
+   validateAccessToken: function(accessToken){
+      // @todo
    }
 };
