d4fb67da2d6cbfdd653c42e3b469e08079c77cd0
Kerem Güneş <k-gun@mail.com>
2016-02-20 02:10:26 +0200

    Update coding style, add comments, remove tmp stuff.

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-var restify = require("restify");
-var restifyErrors = require("restify-errors");
+// import externals
+var _rest = require("restify");
+var _restErr = require("restify-errors");
 
+// import internals
 var app = require("./getir/app.js");
 var util = require("./getir/util/util.js");
 
-// function respond(req, res, next) {
-//    res.send("hello, "+ req.params.name);
-//    next();
-// }
+// server stuff
+var server = _rest.createServer();
+server.use(_rest.bodyParser());
+server.use(_rest.queryParser());
+server.use(_rest.fullResponse());
+server.use(_rest.gzipResponse());
+server.use(_rest.authorizationParser());
+server.use(_rest.acceptParser(server.acceptable));
 
-var server = restify.createServer();
-server.use(restify.bodyParser());
-server.use(restify.queryParser());
-server.use(restify.fullResponse());
-server.use(restify.gzipResponse());
-server.use(restify.authorizationParser());
-server.use(restify.acceptParser(server.acceptable));
-
+// register routes
 server.get("/", app.route.root);
 server.post("/login", app.route.login);
 
+// listen!
 server.listen(3000, function(){
    console.log("Server listening at: %s.", server.url);
 });
