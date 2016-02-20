436bbfcdfc807888c430b2c003b506c1a0af4722
Kerem Güneş <k-gun@mail.com>
2016-02-20 16:23:22 +0200

    Use redis in auth actions.

--- getir/route/post_login.js
+++ getir/route/post_login.js
@@ -2,19 +2,21 @@ var util = require("../util/util.js");
 var appAuth = require("../app_auth.js");
 var appPayload = require("../app_payload.js");
 var appError = require("../app_error.js");
+var appRedis = require("../app_redis.js");
 
 var User = require("../model/user.js");
 
 module.exports = function(req, res, next){
    // @debug
-   // console.log(req.body)
+   console.log(req.body)
 
    var email = util.trim(req.params.email);
    var password = util.trim(req.params.password);
    if (email == "" || password == "") {
       // fill payload error
-      error = appError.get("LOGIN_EMPTY", req);
-      pack  = new appPayload(null, error.code, error.text).pack()
+      var error = appError.get("LOGIN_EMPTY", req);
+      var pack  = new appPayload(null, error.code, error.text).pack();
+
       res.send(400, pack);
       return next();
    }
@@ -27,23 +29,27 @@ module.exports = function(req, res, next){
          console.log(stream.response.toString());
 
          // fill payload error
-         error = appError.get("", req);
-         pack  = new appPayload(null, error.code, error.text).pack()
+         var error = appError.get("", req);
+         var pack  = new appPayload(null, error.code, error.text).pack();
+
          res.send(500, pack);
          return next();
       }
 
       if (data == null || email != data.doc.email || password != data.doc.password) {
          // fill payload error
-         error = appError.get("LOGIN_MATCH", req);
-         pack  = new appPayload(null, error.code, error.text).pack()
+         var error = appError.get("LOGIN_MATCH", req);
+         var pack  = new appPayload(null, error.code, error.text).pack();
+
          res.send(404, pack);
          next();
       } else {
-         // @todo keep access token in memo?
          var accessToken = appAuth.generateAccessToken();
+         // keep access token in memo (expire?)
+         appRedis.setAccessToken(accessToken);
          // pack payload
          pack = new appPayload({"access_token": accessToken}).pack();
+
          res.send(200, pack);
          next();
       }
