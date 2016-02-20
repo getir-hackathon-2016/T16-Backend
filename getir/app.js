c3162aa3e1e236e0ca1df1d2a08c07adb1bea0a4
Kerem Güneş <k-gun@mail.com>
2016-02-20 03:13:00 +0200

    Modularize (separation of business).

--- getir/app.js
+++ getir/app.js
@@ -1,23 +1,8 @@
 var _fs = require("fs");
 
 var app = {};
-app.route = {
-   root: function(req, res, next){
-      res.send("nö!");
-      next();
-   }
-};
 
-// auto require & register routes
-var files = _fs.readdirSync(__dirname +"/route");
-files.forEach(function(file){
-   var fileName = file.slice(0, -3);
-   var filePath = "./route/"+ file;
-
-   // use file name as route name
-   app.route[fileName] = require(filePath);
-
-   console.log("Registering route: '%s' from '%s'.", fileName, filePath)
-});
+app.auth = require("./app_auth.js");
+app.route = require("./app_route.js");
 
 module.exports = app;
--- getir/app_route.js
+++ getir/app_route.js
@@ -4,3 +4,15 @@ module.exports = {
       next();
    }
 };
+
+// auto require & register routes
+var files = _fs.readdirSync(__dirname +"/route");
+files.forEach(function(file){
+   var fileName = file.slice(0, -3);
+   var filePath = "./route/"+ file;
+
+   // use file name as route handler
+   module.exports[fileName] = require(filePath);
+
+   console.log("Registering route: '%s' from '%s'.", fileName, filePath)
+});
