a3ce1ba1febfe56587d089dd96333ad9994e7460
Kerem Güneş <k-gun@mail.com>
2016-02-21 02:45:50 +0200

    Complete GET /root implementation.

--- getir/app_route.js
+++ getir/app_route.js
@@ -1,14 +1,19 @@
-var _fs = require("fs");
+var fs = require("fs");
+var appPayload = require("./app_payload.js");
+var appError = require("./app_error.js");
 
 module.exports = {
    root: function(req, res, next){
-      res.send("nö!");
-      next();
+      // fill payload error
+      var error = appError.get("NO_ROUTE", req);
+      var pack  = new appPayload(null, error.code, error.text).pack();
+      res.send(404, pack);
+      return next();
    }
 };
 
 // auto require & register routes
-var files = _fs.readdirSync(__dirname +"/route");
+var files = fs.readdirSync(__dirname +"/route");
 files.forEach(function(file){
    var fileName = file.slice(0, -3);
    var filePath = "./route/"+ file;
